47f51ff26918dd16b68a347aa22313685c90acbd
koscida <britkos@gmail.com>
2022-07-03 18:52:03 -0400

    keeper v3

--- 33-react/keeper/src/components/Body.js
+++ 33-react/keeper/src/components/Body.js
@@ -1,11 +1,26 @@
+import React, {useState} from 'react'
 import Note from './Note'
-import notes from "../notes";
+import CreateArea from './CreateArea'
+import startingNotes from "../notes";
 
 function Body() {
+	const [notes, setNotes] = useState(startingNotes)
+	
+	const handleNewNote = (newNote) => {
+		const key = notes.length + 1
+		setNotes([...notes, {...newNote, key}])
+	}
+	const handleDeleteNote = (key) => {
+		console.log("key", key)
+		const newList = notes.filter(note => note.key !== key)
+		setNotes(newList)
+	}
+	
 	return <section className="notes p-2">
 		<div className="container-fluid">
+			<CreateArea saveNewNote={handleNewNote} />
 			<div className="card-group row row-cols-2 row-cols-sm-4 row-cols-md-5 row-cols-lg-6 g-4">
-				{notes.map(cardData => <Note cardData={cardData} /> )}
+				{notes.map( cardData => <Note cardData={cardData} key={cardData.key} deleteNote={handleDeleteNote} /> )}
 			</div>
 			
 		</div>
--- 33-react/keeper/src/components/Note.js
+++ 33-react/keeper/src/components/Note.js
@@ -1,9 +1,16 @@
-function Note({cardData: {title, content}}) {
-	return <div class="col">
+function Note({cardData: {key, title, content}, deleteNote}) {
+	
+	const handleDeleteNote = () => {
+		console.log("key", key)
+		deleteNote(key)
+	}	
+	
+	return <div className="col">
 		<div className="card note">
 			<div className="card-body">
-				<h5 class="card-title">{title}</h5>
-				<p class="card-text">{content}</p>
+				<h5 className="card-title">{title}</h5>
+				<p className="card-text">{content}</p>
+				<button onClick={handleDeleteNote}>DELETE</button>
 			</div>
 		</div>
 	</div>
